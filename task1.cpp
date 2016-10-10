a2dc29520f443c91770ee71efb74d372ffa37d16
setlol <dovnar.maxim@gmail.com>
2016-10-10 15:24:36 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+
+void getWashingMachine(int stringSize, struct WashingMachine& washingMachine);
+void printStruct(struct WashingMachine washingMachine);
+struct WashingMachine
+{
+	char* Firm;
+	char* Color;
+	int Sidth;
+	int Sength;
+	int Height;
+	int Power;
+	int SpinSpeed;
+	int HeatingTemperature;
+};
+int main()
+{
+	int stringSize = 256;
+	struct WashingMachine washingMachine;
+
+	washingMachine.Firm = new char[stringSize];
+	washingMachine.Color = new char[stringSize];
+
+	getWashingMachine(stringSize, washingMachine);
+	printStruct(washingMachine);
+
+	delete[] washingMachine.Firm;
+	delete[] washingMachine.Color;
+}
+
+void getWashingMachine(int stringSize, WashingMachine & washingMachine)
+{
+	cout << "enter firm: ";
+	cin.getline(washingMachine.Firm, stringSize);
+	cout << "enter color: ";
+	cin.getline(washingMachine.Color, stringSize);
+	cout << "enter sength: ";
+	cin >> washingMachine.Sength ;
+	cout << "enter sidth: ";
+	cin >> washingMachine.Sidth;
+	cout << "enter spinSpeed: ";
+	cin >> washingMachine.SpinSpeed;
+	cout << "enter HeatingTemperature: ";
+	cin >> washingMachine.HeatingTemperature;
+	cout << "enter Height: ";
+	cin >> washingMachine.Height;
+	cout << "enter Power: ";
+	cin >> washingMachine.Power;
+	cout << endl;
+}
+
+void printStruct(WashingMachine washingMachine)
+{
+	cout << "enter firm: " << washingMachine.Firm << endl << "enter color: " << washingMachine.Color << endl
+	<< "enter sength: " << washingMachine.Sength << endl << "enter sidth: " << washingMachine.Sidth << endl
+	<< "enter spinSpeed: " << washingMachine.SpinSpeed << endl << "enter HeatingTemperature: " << washingMachine.HeatingTemperature << endl
+	<< "enter Height: " << washingMachine.Height << endl << "enter Power: " << washingMachine.Power << endl;
+}
+
+
+/*Реализовать структуру «Стиральная машинка» (фирма, цвет, ширина, длина, высота, мощность, 
+скорость отжима, температура нагрева). Создайте экземпляр структуры и проиллюстрируйте работу с ним.*/
---
+++ task2.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+using namespace std;
+
+void getSmoothingIron(int stringSize, struct SmoothingIron& smoothingIron);
+void printStruct(struct SmoothingIron smoothingIron);
+struct SmoothingIron
+{
+	char* Firm;
+	char* Model;
+	char* Color;
+	int MinimumTemperature;
+	int MaximumTemperature;
+	char* Steam;
+	int Power;
+};
+
+int main()
+{
+	int stringSize = 256;
+	struct SmoothingIron smoothingIron;
+
+	smoothingIron.Firm = new char[stringSize];
+	smoothingIron.Color = new char[stringSize];
+	smoothingIron.Steam = new char[stringSize];
+	smoothingIron.Model = new char[stringSize];
+
+	getSmoothingIron(stringSize, smoothingIron);
+	printStruct(smoothingIron);
+
+	delete[] smoothingIron.Firm;
+	delete[] smoothingIron.Color;
+	delete[] smoothingIron.Steam;
+	delete[] smoothingIron.Model;
+}
+
+void getSmoothingIron(int stringSize, SmoothingIron & smoothingIron)
+{
+	cout << "enter firm: ";
+	cin.getline(smoothingIron.Firm, stringSize);
+	cout << "enter model: ";
+	cin.getline(smoothingIron.Model, stringSize);
+	cout << "enter color: ";
+	cin.getline(smoothingIron.Color, stringSize);
+	cout << "enter minimumTemperature: ";
+	cin >> smoothingIron.MinimumTemperature;;
+	cout << "enter maximumTemperature: ";
+	cin >> smoothingIron.MaximumTemperature;
+	cout << "enter steam if yes y else no n: ";
+	char tempYN;
+	cin >> tempYN;
+	if (tempYN == 'y')
+	{
+		strcpy(smoothingIron.Steam, "Yes");
+	}
+	else
+	{
+		strcpy(smoothingIron.Steam, "No");
+	}
+	cout << "enter Power: ";
+	cin >> smoothingIron.Power;
+	cout << endl;
+}
+
+void printStruct(SmoothingIron smoothingIron)
+{
+	cout << "enter firm: " << smoothingIron.Firm << endl << "enter model: " << smoothingIron.Model << endl
+		<< "enter color: " << smoothingIron.Color << endl << "enter minimum temperature: " << smoothingIron.MinimumTemperature << endl
+		<< "enter maximum temperature: " << smoothingIron.MaximumTemperature << endl << "enter Steam: " << smoothingIron.Steam << endl
+		 << "enter Power: " << smoothingIron.Power << endl;
+}
+
+
+/*Реализовать структуру «Утюг» (фирма, модель, цвет, минимальная температура, максимальная температура, 
+подача пара да/нет, мощность). Создайте экземпляр структуры и проиллюстрируйте работу с ним.*/
